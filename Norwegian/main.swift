c2b22d9ccca6437f081cccf474977f4d87de7c5a
amwoods3 <amwoods3@cougars.ccis.edu>
2017-03-23 11:45:11 +0900

    Initial Commit

---
+++ Norwegian/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Norwegian
+//
+//  Created by Andrew Woods on 2017/03/23.
+//  Copyright © 2017年 Andrew Woods. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
